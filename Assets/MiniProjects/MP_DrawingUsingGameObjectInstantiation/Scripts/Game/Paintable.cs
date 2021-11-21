33addc8fd998890652c268c938daa9083fdda349
George Aleksanian <alexanyanyuka@gmail.com>
2021-11-21 16:58:49 +0200

    Started basic drawing.

--- Assets/MiniProjects/MP_DrawingUsingGameObjectInstantiation/Scripts/Game/Paintable.cs
+++ Assets/MiniProjects/MP_DrawingUsingGameObjectInstantiation/Scripts/Game/Paintable.cs
@@ -1,9 +1,55 @@
 namespace MiniProjects.MP_DrawingUsingGameObjectInstantiation.Scripts.Game
 {
+    using System;
+    using Unity.Mathematics;
     using UnityEngine;
 
     public class Paintable : MonoBehaviour
     {
-    
+        [SerializeField] private GameObject brush;
+        [SerializeField] private float brushSize;
+
+        private Camera mainCamera;
+        private Vector3 lastDrawPosition;
+
+
+        private void Awake()
+        {
+            mainCamera = Camera.main;
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButton(0))
+            {
+                CalculateDrawPosition(() =>
+                {
+                    Draw();
+                });
+            }
+        }
+
+        private void CalculateDrawPosition(Action callback)
+        {
+            var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit raycastHit;
+
+            if (Physics.Raycast(ray, out raycastHit))
+            {
+                lastDrawPosition = raycastHit.point + Vector3.up * 0.1f;
+                
+                callback.Invoke();
+            }
+        }
+
+        private void Draw()
+        {
+            var drawInstance = Instantiate(
+                brush,
+                lastDrawPosition, 
+                quaternion.identity,
+                transform);
+            drawInstance.transform.localScale = Vector3.one * brushSize;
+        }
     }
 }
